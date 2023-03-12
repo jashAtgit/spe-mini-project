82a8647202d1e8eb7cc4dcc9e83239a4709ffc6a
jashAtgit <jash.sai33@gmail.com>
2023-03-12 17:28:26 +0530

    calculator test

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Scientific Calculator App to demonstrate DevOps
 
-![workflow](https://github.com/jashAtgit/spe-mini-project/actions/workflows/ci.yml/badge.svg?branch=master?)
+![workflow](https://github.com/jashAtgit/spe-mini-project/actions/workflows/ci.yml/badge.svg)
 
 This app is built using React <3 <3
--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Calculator.css";
 
-function App() {
+function Calculator() {
+  const [value, setValue] = useState("");
+
+  const handleButtonPress = (e) => {
+    setValue(value + e.target.value);
+  };
+
+  const calculate = () => {
+    try {
+      setValue(eval(value).toString());
+    } catch (error) {
+      setValue("Error");
+    }
+  };
+
+  const clear = () => {
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React YAYYYYYYYYYYY!!!!
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">{value}</div>
+      <div className="buttons">
+        <button onClick={handleButtonPress} value="7">
+          7
+        </button>
+        <button onClick={handleButtonPress} value="8">
+          8
+        </button>
+        <button onClick={handleButtonPress} value="9">
+          9
+        </button>
+        <button onClick={handleButtonPress}  className="operator" value="/">
+          ÷
+        </button>
+        <button onClick={handleButtonPress}  className="function" value="Math.log(">
+          log
+        </button>
+        <button onClick={handleButtonPress}  className="operator" value=")">
+          )
+        </button>
+        <button onClick={handleButtonPress} value="4">
+          4
+        </button>
+        <button onClick={handleButtonPress} value="5">
+          5
+        </button>
+        <button onClick={handleButtonPress} value="6">
+          6
+        </button>
+        <button onClick={handleButtonPress} className="operator" value="*">
+          ×
+        </button>
+        <button onClick={handleButtonPress} value="1">
+          1
+        </button>
+        <button onClick={handleButtonPress} value="2">
+          2
+        </button>
+        <button onClick={handleButtonPress} value="3">
+          3
+        </button>
+        <button onClick={handleButtonPress}  className="operator" value="-">
+          -
+        </button>
+        <button onClick={handleButtonPress} value="0">
+          0
+        </button>
+        <button onClick={handleButtonPress} value=".">
+          .
+        </button>
+        <button onClick={calculate}  className="operator" value="=">
+          =
+        </button>
+        <button onClick={handleButtonPress}  className="operator" value="+">
+          +
+        </button>
+        <button onClick={clear} className="clear" value="AC">
+          AC
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
